09b17ee45605a363ecafca1c0843587cd7b4a8b8
nss <nsosa@uces.edu.ar>
2021-08-30 09:14:30 -0300

    Creando wp en docker

---
+++ terraform-docker-wordpress/README.md
@@ -0,0 +1,7 @@
+## Ejemplo para crear un wordpress en docker usando terraform
+
+Básicamente crea dos contenedores, la base y la que contiene el wp.
+
+Publicado en el puerto 8080.
+
+
---
+++ terraform-docker-wordpress/docker.tf
@@ -0,0 +1,40 @@
+provider "docker" {
+    
+    host = "unix:///var/run/docker.sock"
+}
+
+# Create the network
+resource "docker_network" "private_network" {
+    name = "wp_network"
+}
+
+# create db container
+resource "docker_container" "db" {
+  name  = "db"
+  image = "mysql:5.7"
+  env = ["MYSQL_DATABASE=wordpress", "MYSQL_USER=nico", "MYSQL_PASSWORD=nico", "MYSQL_RANDOM_ROOT_PASSWORD=nico"]
+  command = ["--default-authentication-plugin=mysql_native_password"]
+  network_mode = "bridge"
+
+  networks_advanced {
+    name    = "wp_network"
+  }
+}
+
+# create wordpress container
+resource "docker_container" "wordpress" {
+  name  = "wordpress"
+  image = "wordpress:latest"
+  env = ["WORDPRESS_DB_HOST=db", "WORDPRESS_DB_USER=nico", "WORDPRESS_DB_PASSWORD=nico", "WORDPRESS_DB_NAME=wordpress"]
+  network_mode = "bridge"
+
+  networks_advanced {
+    name    = "wp_network"
+  }
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
